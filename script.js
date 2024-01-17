891f87559e27d24a2c6bf8c547ca339eb222849c
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2024-01-17 11:47:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function makeid(l) {
-  // write your code here
-}
-
-// Do not change the code below.
+    // write your code here
+      let len = 1;
+      let str = "";
+      while(len <= l){
+          let random = Math.floor(Math.random()*127);
+          
+          if((random >= 48 && random <= 57) || (random >= 65 && random <= 90) || (random >= 97 && random <= 122)){
+            str = str + String.fromCharCode(random);
+          len++;
+          } 
+      }
+      return str;
+  }
 const l = prompt("Enter a number.");
 alert(makeid(l));
